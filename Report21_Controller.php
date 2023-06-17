76ead9ac3eda72bdbf76845e78c58322538ff66f
Yura N <winnypuch@gmail.com>
2023-06-17 19:17:52 +0300

    l11

--- Report21_Controller.php
+++ Report21_Controller.php
@@ -25,9 +25,17 @@ $sDateTimeX1 = "---";
 $sTeacherFioFactD2 = "---";
 $sDepartmentNameH2 = "---";
 $sDepartmentAddressL2 = "---";
+$sCabinetH1 = "---";
+$sLessonTimeX1 = "---";
 $sProgramAgeX2 = "---";
 $sAcademicYearU2 = "---";
-
+$sWeekLessonR2 = "---";
+$sProgramForYearL3 = "---";
+$sSubsectionP3 = "---";
+$sJobNameT3 = "---";
+$sPrintOutsPdf = "---";
+$aTeachers =[];
+$vTeachers = [];
 
 //методисты 2
 //педагоги таблица  520
@@ -76,26 +84,24 @@ $sSqlQueryGroup = "SELECT DISTINCT
         Groups.id AS GroupId
         , Groups.f11090 AS GroupName
         , if(Groups.f11140 IS NULL, '', Groups.f11140) AS ProgramAge
-        , if(Schedule.f13580 IS NULL, '', Schedule.f13580) AS FormatPlan
-        , if(Schedule.f13630 IS NULL, '', Schedule.f13630) AS FormatPlan
+        , if(Teacher.id IS NULL, 0, Teacher.id) AS TeacherId
         , if(Teacher.f9660 IS NULL, '', Teacher.f9660) AS TeacherFioFact
         , if(Departments.f9450 IS NULL, '', Departments.f9450) AS DepartmentName
         , if(Departments.f9460  IS NULL, '', Departments.f9460) AS DepartmentAddress
         , IFNULL((SELECT MAX(Classes.f12750) FROM " . DATA_TABLE . get_table_id(780) . "AS Classes  WHERE Classes.f12870 = Groups.id AND Classes.status = 0),'') AS MaxClassesDate
         , if(Groups.f11240 IS NULL, '', Groups.f11240) AS ClassStartDate
     FROM
-        " . DATA_TABLE . get_table_id(720) . " AS Students
+        " . DATA_TABLE . get_table_id(790) . " AS Schedule
         INNER JOIN " . DATA_TABLE . get_table_id(700) . " AS Groups
-            ON Students.f11570 = Groups.id
-        LEFT JOIN " . DATA_TABLE . get_table_id(790) . " AS Schedule
             ON Schedule.f13550 = Groups.id
         LEFT JOIN " . DATA_TABLE . get_table_id(520) . " AS Teacher
             ON Groups.f11180 = Teacher.id
         LEFT JOIN " . DATA_TABLE . get_table_id(500) . " AS Departments
             ON Groups.f11150 = Departments.id
     WHERE
-    (Groups.f11240 <>'" . $sDateZero . "' AND NOT Groups.f11240 IS NULL AND (Groups.f11270 IS NULL OR Groups.f11270 = '" . $sDateZero . "'
-        OR (Groups.f11270 <>'" . $sDateZero . "' AND NOT Groups.f11270 IS NULL AND DATE_ADD(Groups.f11270, INTERVAL 30 DAY) >= '" . $sDateNow . "')))
+        Schedule.status = 0
+        AND (Groups.f11240 <>'" . $sDateZero . "' AND NOT Groups.f11240 IS NULL AND (Groups.f11270 IS NULL OR Groups.f11270 = '" . $sDateZero . "'
+            OR (Groups.f11270 <>'" . $sDateZero . "' AND NOT Groups.f11270 IS NULL AND DATE_ADD(Groups.f11270, INTERVAL 30 DAY) >= '" . $sDateNow . "')))
         AND Groups.f11260 = '0'
         AND Groups.status = 0";
 //проверка грыппы на пользователя педагога
@@ -105,127 +111,185 @@ if ($iTeacherId != 0) {
 
 $sSqlQueryGroup = $sSqlQueryGroup . " ORDER BY GroupName";
 
-$vGroupData = sql_query($sSqlQueryGroup);
+if($bdebug) echo $sSqlQueryGroup."--3<br>";
+
+
 $vGroups = [];
-
-while ($vGroupRow = sql_fetch_assoc($vGroupData)) {
-    if ($iGroupId == 0) {
-        $iGroupId = $vGroupRow['GroupId'];
+$iGroupTeacherId = 0;
+if($vGroupData = sql_query($sSqlQueryGroup)){
+    while ($vGroupRow = sql_fetch_assoc($vGroupData)) {
+        if ($iGroupId == 0) {
+            $iGroupId = $vGroupRow['GroupId'];
+        }
+        $vTableData['GroupId'] = $vGroupRow['GroupId'];
+        $vTableData['GroupName'] = form_display($vGroupRow['GroupName']);
+        if ($iGroupId == $vGroupRow['GroupId']) {
+            $sProgramAgeX2 = $vGroupRow['ProgramAge'];
+            $sTeacherFioFactD2 = form_display($vGroupRow['TeacherFioFact']);
+            $sDepartmentNameH2 = form_display($vGroupRow['DepartmentName']);
+            $sDepartmentAddressL2 = form_display($vGroupRow['DepartmentAddress']);
+            $sMaxClassesDate = $vGroupRow['MaxClassesDate'];
+            $sClassStartDate = $vGroupRow['ClassStartDate'];
+            $iGroupTeacherId = $vGroupRow['TeacherId'];
+        }
+        $vTableData['TeacherId'] = $vGroupRow['TeacherId'];
+        $vTableData['ProgramAge'] = $vGroupRow['ProgramAge'];
+        $vTableData['sDepartmentNameH2'] = $vGroupRow['sDepartmentNameH2'];
+        $vTableData['sDepartmentAddressL2'] = $vGroupRow['sDepartmentAddressL2'];
+        $vTableData['MaxClassesDate'] = $vGroupRow['MaxClassesDate'];
+        $vTableData['ClassStartDate'] = $vGroupRow['ClassStartDate'];
+        $vTableData['TeacherFioFact'] = $vGroupRow['TeacherFioFact'];
+        $vGroups['g'.$vGroupRow['GroupId']] = $vTableData;
     }
-    $vTableData['GroupId'] = $vGroupRow['GroupId'];
-    $vTableData['GroupName'] = form_display($vGroupRow['GroupName']);
-    if ($iGroupId == $vGroupRow['GroupId']) {
-        $sProgramAgeX2 = form_display($vGroupRow['ProgramAge']);
-        $sFormatPlanL1 = form_display($vGroupRow['FormatPlan']);
-        $sTeacherFioFactD2 = form_display($vGroupRow['TeacherFioFact']);
-        $sDepartmentNameH2 = form_display($vGroupRow['sDepartmentNameH2']);
-        $sDepartmentAddressL2 = form_display($vGroupRow['sDepartmentAddressL2']);
-        $sMaxClassesDate = $vGroupRow['MaxClassesDate'];
-        $sClassStartDate = $vGroupRow['ClassStartDate'];
-    }
-    $vTableData['ProgramAge'] = form_display($vGroupRow['ProgramAge']);
-    $vTableData['FormatPlan'] = form_display($vGroupRow['FormatPlan']);
-    $vTableData['TeacherFioMin'] = $vGroupRow['TeacherFioMin'];
-    $vGroups['g'.$vGroupRow['GroupId']] = $vTableData;
 }
-$sAcademicYearU2 = "";
 
 
-
-//Работа с таблице параметров отчёта
-$iReportId = 410; //Id текущего отчёта
-$aReportData = [];
-//Данные для отчётоВ 970
-if ($vResReportData = sql_query("SELECT f17790 FROM " . DATA_TABLE . get_table_id(970) ." WHERE f17780='" . $iReportId . "' AND status='0'")) {
-    if ($vRowReportData = sql_fetch_assoc($vResReportData)) {
-        $aReportData = json_decode($vRowReportData['f17790'], true);
-        if($bdebug) echo $iReportId."--1<br>";
-        if(!array_key_exists("iFaultData", $aReportData)){
-            if($bdebug) echo $iReportId."--2<br>";
-            $aReportData = ["iFaultData"=> 2.00, "iMinQtyClasses" => 10, "iMinQtyClassesSubdivision" => 3];
-            data_update(970, array('status'=>'0', 'f17790'=>json_encode($aReportData)), "`f17780`='",$iReportId,"' AND status ='0'" );
+//$aDays[date("w", strtotime($sDateSearch))]
+//Находим следующую дату занятия
+//$aDaysEn = array('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
+//$aDays = array('Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота');
+$aDaysToEng = array('Воскресенье' => 'sunday', 'Понедельник' => 'monday', 'Вторник' => 'tuesday', 'Среда' => 'wednesday', 'Четверг' => 'thursday', 'Пятница' => 'friday', 'Суббота' => 'saturday');
+if($sMaxClassesDate == ""){
+    $sStartDate = $sClassStartDate;
+}else{
+    $sStartDate = $sMaxClassesDate;
+}
+$dStartDate = new DateTime($sStartDate);
+$sStartWeekDay = strtolower(date("l", strtotime($sStartDate)));
+$dSearchDate = $dStartDate;
+$iWeekDay = 0;
+$sWeekDayV1 = "";
+//$dStartDate->modify('next monday');
+//Расписние 790 Дни недели 600
+//f13560 День недели
+//f10330 Дни недели
+$iColClass = 0;
+if ($vResWeekDays = sql_query("SELECT WeekDays.id AS WeekDayId, WeekDays.f10330 AS WeekDayName FROM " . DATA_TABLE . get_table_id(790) ." AS Schedule INNER JOIN " . DATA_TABLE . get_table_id(600) ." AS WeekDays ON Schedule.f13560 = WeekDays.id WHERE Schedule.f13550='" . $iGroupId . "' AND Schedule.status='0' ORDER BY WeekDays.id")) {
+    while ($vRowWeekDays = sql_fetch_assoc($vResWeekDays)) {
+        $iColClass++;
+        //Если день начальной даты для поиска равен дню недели
+        if($sStartWeekDay == $aDaysToEng[$vRowWeekDays['WeekDayName']]) {
+            $iWeekDay = $vRowWeekDays['WeekDayId'];
+            $sWeekDayV1 = $vRowWeekDays['WeekDayName'];
+            $dSearchDate = $dStartDate;
+            break;
+        } else{
+            if($iColClass == 1) {
+                $dSearchDate->modify('next '.$aDaysToEng[$vRowWeekDays['WeekDayName']]);
+                $iWeekDay = $vRowWeekDays['WeekDayId'];
+                $sWeekDayV1 = $vRowWeekDays['WeekDayName'];
+            }else{
+                $dCheckDate = $dStartDate;
+                $dCheckDate->modify('next '.$aDaysToEng[$vRowWeekDays['WeekDayName']]);
+                if($dCheckDate < $dSearchDate) {
+                    $dSearchDate = $dCheckDate;
+                    $iWeekDay = $vRowWeekDays['WeekDayId'];
+                    $sWeekDayV1 = $vRowWeekDays['WeekDayName'];
+                }
+            }
         }
     }
 }
-
-if(!array_key_exists("iFaultData", $aReportData)){
-    if($bdebug) echo $iReportId."--3<br>";
-    $aReportData = ["iFaultData"=> 2.00, "iMinQtyClasses" => 10, "iMinQtyClassesSubdivision" => 3];
-    data_insert(970, array('status'=>'0', 'f17780'=>$iReportId, 'f17790'=>json_encode($aReportData)));
-}
-//сохраняем данные
-if (array_key_exists('iFaultData', $_REQUEST) && array_key_exists('iMinQtyClasses', $_REQUEST) && array_key_exists('iMinQtyClassesSubdivision', $_REQUEST)) {
-    if($bdebug) echo $iReportId."--4<br>";
-    if(floatval($aReportData['iFaultData']) != floatval($_REQUEST['iFaultData']) || intval($aReportData['iMinQtyClasses']) != intval($_REQUEST['iMinQtyClasses']) || intval($aReportData['iMinQtyClassesSubdivision']) != intval($_REQUEST['iMinQtyClassesSubdivision'])){
-        if($bdebug) echo $iReportId."--5<br>";
-        $aReportData = ["iFaultData"=> floatval($_REQUEST['iFaultData']), "iMinQtyClasses" => intval($_REQUEST['iMinQtyClasses']), "iMinQtyClassesSubdivision" => intval($_REQUEST['iMinQtyClassesSubdivision'])];
-        data_update(970, array('f17790'=>json_encode($aReportData)), "`f17780`='",$iReportId,"' AND status ='0' ");
-    }
-}
-
-
-
-//f11480 Название группы факт f11580 Дата зачисления f11590 Дата отчисления
-$sSqlQueryStudents = "SELECT ClienCards.id as ChildrenId
-    , ClienCards.f9750 as ChildrenFIO
-    , Groups.f11090 AS GroupFactName
-FROM
-" . DATA_TABLE . get_table_id(720) . " AS Students
-    INNER JOIN " . DATA_TABLE . get_table_id(530) . " AS ClienCards
-        ON Students.f11460 = ClienCards.id
-    INNER JOIN " . DATA_TABLE . get_table_id(700) . " AS Groups
-        ON Students.f11480 = Groups.id
+// Если количество занятий больше 0
+if($iColClass > 0){
+    $sSqlQuerySchedule = "SELECT
+       if(Schedule.f13580 IS NULL, '', Schedule.f13580) AS FormatPlan
+        , if(Schedule.f13630 IS NULL, '', Schedule.f13630) AS Cabinet
+        , if(Schedule.f13570 IS NULL, '', Schedule.f13570) AS LessonTime
+    FROM
+        " . DATA_TABLE . get_table_id(790) . " AS Schedule
+        INNER JOIN " . DATA_TABLE . get_table_id(700) . " AS Groups
+            ON Schedule.f13550 = Groups.id
     WHERE
-        Students.f11480 = '" . $iGroupId . "'
-        AND ((Students.f11580 <='" . $sDateEnd . "' AND Students.f11590 IS NULL AND Students.f11580 <>'" . $sDateZero . "')
-        OR (Students.f11580 <='" . $sDateEnd . "' AND Students.f11590 = '" . $sDateZero . "' AND Students.f11580 <>'" . $sDateZero . "')
-        OR (Students.f11580 <>'" . $sDateZero . "' AND Students.f11580 <='" . $sDateEnd . "' AND Students.f11590 >= '" . $sDateBeg . "' AND Students.f11590 <>'" . $sDateZero . "'))
-        AND Students.status = 0
-    ORDER BY ChildrenFIO";
-//                AND GradesClass.f17280 ='".$vGroups['g'.$iGroupId]['ClassFormat']."'
-if($vStudentsData = sql_query($sSqlQueryStudents)) {
-    while ($vStudentRow = sql_fetch_assoc($vStudentsData)) {
-        $iQtyClasses = 0;
-    //810 Оценки на занятии f14680 ФИО ребенка f17280 Формат Факт f17260 Пр. возраст Дата f14820 Был? f14700
-        $sSqlQueryGradesClass = "SELECT IFNULL(COUNT(*), 0) AS QtyClasses FROM " . DATA_TABLE . get_table_id(810) . " AS GradesClass
-            WHERE
-                GradesClass.f14680 = ".$vStudentRow['ChildrenId']."
-                AND GradesClass.f17260 ='".$vGroups['g'.$iGroupId]['ProgramAge']."'
-                AND GradesClass.f14820 >= '".$sDateBeg."' AND GradesClass.f14820 <= '".$sDateEnd."'
-                AND if(GradesClass.f14700 IS NULL, '', GradesClass.f14700) = ''
-                AND GradesClass.status = 0";
-        if($vResultGradesClass = sql_query($sSqlQueryGradesClass)) {
-            if($vRowGradesClass = sql_fetch_assoc($vResultGradesClass)){
-                //echo $vGroupRow['GroupId']."___".$iWorkingOff."---".$vRowMisses['MissesCount']."---<br>";
-                $iQtyClasses = $vRowGradesClass ['QtyClasses'];
+        Schedule.f12870 = ".$iGroupId."
+        AND Schedule.f13560 = ".$iWeekDay."
+        AND Schedule.status = 0";
+    if($vScheduleData = sql_query($sSqlQuerySchedule)){
+        while ($vScheduleRow = sql_fetch_assoc($vScheduleData)) {
+            $sFormatPlanL1 = form_display($vScheduleRow['FormatPlan']);
+            $sCabinetH1 = form_display($vScheduleRow['Cabinet']);
+            $sLessonTimeX1 = form_display($vScheduleRow['LessonTime']);
+        }
+    }
+    //Учебный год
+    $dTrainingBeg = new DateTime();
+    $dTrainingBeg->setDate(intval($dSearchDate->format("Y")) - 1, 9, 1);
+    $dTrainingEnd = new DateTime();
+    $dTrainingEnd->setDate(intval($dSearchDate->format("Y")), 8, 31);
+
+    $sAcademicYearU2 = ($dSearchDate >= $dTrainingBeg && $dSearchDate <= $dTrainingEnd) ?  strval(intval($dSearchDate->format("Y")) - 1) : $dSearchDate->format("Y");
+    $iWeek = 0;
+    $iLesson = 0;
+    //Из таблицы Занятия 780 по полю Название группы f12870 вычисляем по полям № недели f13020 и № урока f13340 номер последнего занятия (сначала макс по неделе, потом макс по уроку).
+    if ($vResClassesData = sql_query("SELECT MAX(f13020) AS Week FROM " . DATA_TABLE . get_table_id(780) ." WHERE f12870='" . $iGroupId . "' AND status='0'")) {
+        if ($vRowClassesData = sql_fetch_assoc($vResClassesData)) {
+            $iWeek = intval($vRowClassesData['Week']);
+            if ($vResLessonData = sql_query("SELECT MAX(f13340) AS Lesson FROM " . DATA_TABLE . get_table_id(780) ." WHERE f13020 = '".$iWeek."' AND f12870='" . $iGroupId . "' AND status='0'")) {
+                if ($vRowLessonData = sql_fetch_assoc($vResLessonData)) {
+                    $iLesson = intval($vRowLessonData['Week']);
+                    if($iColClass == 1) {
+                        $iWeek++;
+                        $iLesson = 1;
+                    } else {
+                        if($iLesson == 1) {
+                            $iLesson++;
+                        } else {
+                            $iWeek++;
+                            $iLesson = 1;
+                        }
+                    }
+                    $sWeekLessonR2 = strval($iWeek).".".strval($iLesson);
+                }
             }
         }
-        $sSqlReportState = "SELECT Id AS ReportStateId, f17840 AS UniqueId, f17940 AS ReportState
-            FROM " . DATA_TABLE . get_table_id(980) ."
-            WHERE
-                f17850='" . $vStudentRow['ChildrenId'] . "'
-                AND f17860='" . $iGroupId . "'
-                AND f17870='" . $sDateBeg . "'
-                AND f17880='" . $sDateEnd . "'
-                AND f17890='" . floatval($aReportData['iFaultData']) . "'
-                AND f17900='" . intval($aReportData['iMinQtyClasses']) . "'
-                AND f17910='" . intval($aReportData['iMinQtyClassesSubdivision'])  . "'
-                AND f17920='" . ($bIsPerfomance ? "1": "0") . "'
-                AND status='0'";
-        $iReportStateId = 0;
-        $iReportState = -1;
-        $sReportUniqueId = "";
-        if ($vResReportState = sql_query($sSqlReportState)) {
-            if ($vRowReportState = sql_fetch_assoc($vResReportState)) {
-                $iReportStateId = $vRowReportState['ReportStateId'];
-                $sReportUniqueId = $vRowReportState['UniqueId'];
-                $iReportState = $vRowReportState['ReportState'];
-                if($bdebug) echo "vResReportState1--1<br>";
-            }
+    }
+
+    $sSqlQueryTeachers = "SELECT
+           Teachers.id AS TeacherId
+           , Teachers.f9660 AS TeacherName
+        FROM
+            " . DATA_TABLE . get_table_id(520) . " AS Teachers
+        WHERE
+           Teachers.id <> ".$iGroupTeacherId."
+            AND Teachers.status = 0
+        ORDER BY Teachers.f9660";
+    if($vTeachersData = sql_query($sSqlQueryTeachers)){
+        while ($vTeachersRow = sql_fetch_assoc($vTeachersData)) {
+            $aTeachers["TeacherId"] = $vTeachersRow["TeacherId"];
+            $aTeachers["TeacherName"] = $vTeachersRow["TeacherName"];
+            $vTeachers[] = array("TeacherId" => $vTeachersRow["TeacherId"], "TeacherName" => $vTeachersRow["TeacherName"]);
         }
+    }
 
-
-        $vLines[] = array("ChildrenId" => $vStudentRow['ChildrenId'], "ChildrenFIO"=> $vStudentRow['ChildrenFIO'], "QtyClasses" => $iQtyClasses, "ReportState" => $iReportState, "ReportStateId" => $iReportStateId, "ReportUniqueId" => $sReportUniqueId);
+    $sSqlQueryProgramForYear = "SELECT
+           ProgramForYear.f12600 AS ProgramForYearName
+        FROM
+            " . DATA_TABLE . get_table_id(730) . " AS ProgramForYear
+        WHERE
+           ProgramForYear.f11700 = '".$sProgramAgeX2."'
+           AND ProgramForYear.f11710 = '".$iWeek."'
+           AND ProgramForYear.f11850 = '".$iLesson."'
+           AND ProgramForYear.status = 0";
+    if($vProgramForYearData = sql_query($sSqlQueryProgramForYear)){
+        if ($vProgramForYearRow = sql_fetch_assoc($vProgramForYearData)) {
+            $sProgramForYearL3 = $vProgramForYearRow['ProgramForYearName'];
+        }
+    }
+    $sSqlQueryTasks = "SELECT
+           Tasks.f14840 AS Subsection
+           , Tasks.f10440 AS JobName
+            , Tasks.f10480 AS PrintOutsPdf
+        FROM
+            " . DATA_TABLE . get_table_id(620) . " AS Tasks
+        WHERE
+           Tasks.f12530 = '".$sProgramForYearL3."'
+           AND Tasks.status = 0";
+    if($vProgramForYearData = sql_query($sSqlQueryProgramForYear)){
+        if ($vProgramForYearRow = sql_fetch_assoc($vProgramForYearData)) {
+            $sSubsectionP3 = $vProgramForYearRow['Subsection'];
+            $sJobNameT3 = $vProgramForYearRow['JobName'];
+            $sPrintOutsPdf = $vProgramForYearRow['PrintOutsPdf'];
+        }
     }
 }
 
@@ -245,7 +309,7 @@ $smarty->assign("sDateTimeX1", $sDateTimeX1);
 $smarty->assign("sTeacherFioFactD2", $sTeacherFioFactD2);
 $smarty->assign("sDepartmentNameH2", $sDepartmentNameH2);
 $smarty->assign("sDepartmentAddressL2", $sDepartmentAddressL2);
-
+$smarty->assign("aTeachers", $aTeachers);
 $smarty->assign("sAcademicYearU2", $sAcademicYearU2);
 $smarty->assign("sProgramAgeX2", $sProgramAgeX2);
 
